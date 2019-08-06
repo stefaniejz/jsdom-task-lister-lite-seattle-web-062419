a248f6b5c3b48ee244888f7519dff8d040929ba0
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-08-06 13:16:47 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form")
+  form.addEventListener("submit", function(event){
+      event.preventDefault()
+
+      const newtask = document.createElement("li")
+      const list = document.getElementById("tasks")
+      const userInput = document.getElementById("new-task-description")
+
+      newtask.textContent = userInput.value
+      list.appendChild(newtask)
+
+
+  
+     
+     
+  });
 });
